167c1ed6f0e8c7dfe7c0056592906d7236d84034
450294533@qq.com <chenshuguang1234>
2017-07-12 01:45:11 +0800

    修改播放详情页

--- spring-common-utils/src/main/java/com/csg/tools/JsoupUtils.java
+++ spring-common-utils/src/main/java/com/csg/tools/JsoupUtils.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Created by Silence on 2017/1/25.
  */
 public class JsoupUtils {
-  private static final String UA_PHONE = "Mozilla/5.0 (Linux; Android 4.3; Nexus 10 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Safari/537.36";
+  private static final String UA_PHONE = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Mobile Safari/537.36";
   private static final String UA_PC = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
   private static final int TIME_OUT = 10 * 1000;
 
